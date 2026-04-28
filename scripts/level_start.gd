9395c5da76d7a8194cf27ba7f958db5836122ac9
Jupresson <jaaralasse9@gmail.com>
2026-04-28 13:15:42 +0300

    Add level start script and UI audio tweaks
    Add scripts/level_start.gd (and UID) and attach it to scenes/levels/level_0.tscn to enable input when a level is ready. Enhance UI audio behavior: preload and play a UI click sound from ui/scripts/start_menu.gd, stop all audio on scene transitions/quit, and add helper to send AudioManager requests. Adjust SceneReadySound nodes in ui/scenes/options_ui.tscn and ui/scenes/start_ui.tscn (unique tags, stream resources, and search_recursively flag). Update editor metadata/layout to open/select the new scenes and update recent files. (Also updates .godot/uid_cache.bin.)

---
+++ scripts/level_start.gd
@@ -0,0 +1,6 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	InputManager.set_input_enabled(true)
--- ui/scripts/start_menu.gd
+++ ui/scripts/start_menu.gd
@@ -3,16 +3,43 @@ extends Control
 const OPTIONS_SCENE_PATH := "res://ui/scenes/options_ui.tscn"
 const START_SCENE_PATH := "res://scenes/levels/level_0.tscn"
 
+
+
+const STREAM_PATH: AudioStream = preload("res://assets/sounds/sfx/general_ui/ui_click.ogg")
+var bus_category: AudioManager.BusCategory = AudioManager.BusCategory.UI
+var volume_db: float = -12.0
+var pitch_range: Vector2 = Vector2(1.0, 1.0)
+var unique_tag: StringName = &""
+var category: StringName = &""
+var fade_out_seconds: float = 0.15
+var playback_kind: AudioManager.PlaybackKind = AudioManager.PlaybackKind.GLOBAL_2D
+
 func _ready():
-	InputManager.set_input_enabled(true)
-	
+	InputManager.set_input_enabled(false)
+
 func _on_start_pressed() -> void:
+	_play_ui_click_stream_sound()
+	AudioManager.stop_all()
 	SceneManager.change_scene_with_loading(START_SCENE_PATH, 2.0)
 
 
 func _on_settings_pressed() -> void:
+	_play_ui_click_stream_sound()
 	SceneManager.change_scene(OPTIONS_SCENE_PATH)
 
 
 func _on_quit_pressed() -> void:
+	_play_ui_click_stream_sound()
+	AudioManager.stop_all()
 	get_tree().quit()
+
+func _play_ui_click_stream_sound() -> void:
+	var request := AudioManager.SoundRequest.new()
+	request.stream = STREAM_PATH
+	request.playback_kind = playback_kind
+	request.bus_category = bus_category
+	request.volume_db = volume_db
+	request.pitch_range = pitch_range
+	request.unique_tag = unique_tag
+	request.fade_out_seconds = fade_out_seconds
+	AudioManager.play_sound(request)
